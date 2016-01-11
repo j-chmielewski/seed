3bbb8889ddf5c587de2789b1b16f693e9787d982
Jacek Chmielewski <jchmielewski@teonite.com>
2016-01-11 07:40:25 +0100

    tests + refactored to class

--- seed.py
+++ seed.py
@@ -8,59 +8,66 @@ from colorama import init, Fore, Style
 
 SEED_LIST = join(expanduser('~'), '.seedlist')
 
-def print_help():
-    print('TODO: help message')
+
+class Seed(object):
+
+    def __init__(self, seed_list=SEED_LIST):
+        self.seed_list = seed_list
+
+    def print_help(self):
+        print('TODO: help message')
 
 
-def get_seeds():
-    repos = []
-    with open(SEED_LIST, 'r') as seed_list:
-        for path in seed_list:
-            try:
-                repos.append(Repo(path.strip()))
-            except NoSuchPathError:
-                print("{}WARN: Path {} is not a valid git repository{}".format(Fore.RED, path.strip(), Style.RESET_ALL))
+    def get_seeds(self):
+        repos = []
+        with open(self.seed_list, 'r') as seed_list:
+            for path in seed_list:
+                try:
+                    repos.append(Repo(path.strip()))
+                except NoSuchPathError:
+                    print("{}WARN: Path {} is not a valid git repository{}".format(Fore.RED, path.strip(), Style.RESET_ALL))
 
-    seeds = {}
-    for repo in repos:
-        for tag in repo.tags:
-            if str(tag).startswith('seed'):
-                seeds[tag.commit] = tag
+        seeds = {}
+        for repo in repos:
+            for tag in repo.tags:
+                if str(tag).startswith('seed'):
+                    seeds[tag.commit] = tag
 
-    return seeds
-
-
-def print_seeds():
-    seeds = get_seeds()
-    for commit, tag in seeds.items():
-        print("{}{}\t{}{}\t{}{}{}".format(Fore.CYAN, commit, Fore.YELLOW, str(tag), Fore.WHITE, tag.tag.message, Style.RESET_ALL))
+        return seeds
 
 
-def add_seed():
-    # TODO
-    print("{}Add seed: not implemented{}".format(Fore.RED, Style.RESET_ALL))
+    def print_seeds(self):
+        seeds = self.get_seeds()
+        for commit, tag in seeds.items():
+            print("{}{}\t{}{}\t{}{}{}".format(Fore.CYAN, commit, Fore.YELLOW, str(tag), Fore.WHITE, tag.tag.message, Style.RESET_ALL))
 
 
-def plant_seed(seed_id=None):
-    if not seed_id:
-        print("{}Missing seed id{}\n".format(Fore.RED, Style.RESET_ALL))
-        print_help()
-        sys.exit(1)
+    def add_seed(self):
+        # TODO
+        print("{}Add seed: not implemented{}".format(Fore.RED, Style.RESET_ALL))
+
+
+    def plant_seed(self, seed_id=None):
+        if not seed_id:
+            print("{}Missing seed id{}\n".format(Fore.RED, Style.RESET_ALL))
+            self.print_help()
+            sys.exit(1)
 
 
 if __name__ == '__main__':
     init()
+    seed = Seed()
     if len(sys.argv) < 2:
-        print_help()
+        seed.print_help()
         sys.exit(1)
 
     command = sys.argv[1]
     if command == 'list':
-        print_seeds()
+        seed.print_seeds()
     elif command == 'add':
-        add_seed(*sys.argv[2:])
+        seed.add_seed(*sys.argv[2:])
     elif command == 'plant':
-        plant_seed(*sys.argv[2:])
+        seed.plant_seed(*sys.argv[2:])
     else:
-        print_help()
+        seed.print_help()
         exit(1)
---
+++ tests.py
@@ -0,0 +1,33 @@
+import unittest
+
+from seed import Seed
+
+
+class TestSeed(unittest.TestCase):
+
+    def setUp(self):
+        self.seed = Seed(seed_list='./testing/seedlist')
+
+    def test_print_help(self):
+        self.assertIsNone(self.seed.print_help())
+
+
+    def test_get_seeds(self):
+        self.assertIsNotNone(self.seed.get_seeds())
+
+
+    def test_print_seeds(self):
+        self.assertIsNone(self.seed.print_seeds())
+
+
+    def test_add_seed(self):
+        self.assertIsNone(self.seed.add_seed())
+
+
+    def test_plant_seed(self):
+        with self.assertRaises(SystemExit):
+            self.seed.plant_seed()
+
+
+if __name__ == '__main__':
+    unittest.main(buffer=True)
